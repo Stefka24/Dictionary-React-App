a0f5cd062b105eb281b8729e36191402cb3da1a1
Stefka24 <84857822+Stefka24@users.noreply.github.com>
2021-07-08 18:17:20 +0100

    added open source

--- src/index.js
+++ src/index.js
@@ -7,6 +7,13 @@ import reportWebVitals from "./reportWebVitals";
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <p>
+      {" "}
+      <a href="https://github.com/Stefka24/Dictionary-React-App">
+        Open source
+      </a>{" "}
+      by Stefka Bodurova
+    </p>
   </React.StrictMode>,
   document.getElementById("root")
 );
